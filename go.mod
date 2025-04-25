2d3de29f9fdc551afc782599c1fed6be29ab3eec
Kanako <kanakotjapan@gmail.com>
2025-04-24 17:04:19 -0700

    ch1 L4

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kanakotjapan/chirpy.git
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	
+	server := &http.Server{
+	Addr: ":8080",
+	Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err!= nil {
+		fmt.Printf("error starting server: %s\n", err)
+	}
+}
