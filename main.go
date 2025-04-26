4ef22f9581deffae89a3ee5b9b3f344da596094e
Kanako <kanakotjapan@gmail.com>
2025-04-25 23:38:59 -0700

    Ch1 L12

--- main.go
+++ main.go
@@ -7,15 +7,20 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
 	fmt.Println("Sever running at http://localhost:8080")
+
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
