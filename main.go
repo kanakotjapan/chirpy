5e38788f73ef4e9a8e50bf3a54e19864297e13c8
Kanako <kanakotjapan@gmail.com>
2025-04-25 22:27:12 -0700

    Ch1 L7

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	server := &http.Server{
-	Addr: ":8080",
-	Handler: mux,
+		Addr:    ":8080",
+		Handler: mux,
 	}
 
+	fmt.Println("Sever running at http://localhost:8080")
 	err := server.ListenAndServe()
-	if err!= nil {
+	if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 	}
 }
